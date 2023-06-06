eec4aeab1ecca319b6d713d40fd324721addb08b
Caleb Beazel <caleb.beazel@gmail.com>
2023-06-06 11:56:31 -0600

    part 2 complete

--- part-2/part_2.py
+++ part-2/part_2.py
@@ -1,31 +1,34 @@
 ### Step 1 - Lists ###
 
 # Fill in this list with several authors you are a fan of. At least 7 or 8 should do.
-my_authors = []
+my_authors = ["Alfred Lansing", "David McCullough", "Brandon Sanderson", "J K Rowling", "Viktor Frankl", "Ralph Waldo Emerson", "James Clear"]
 
 # Now let's add a new author to the end with the .append() method. Type your code below.
-
 # Code here
 # Example: my_authors.append("H.G. Wells")
 
+my_authors.append("Brene Brown")
 
 # Now let's remove an element in the list
 
 # Code here
 # Example: my_authors.remove("H.G. Wells")
 
+my_authors.remove("Brene Brown")
 
 # Now access an element by it's index. (Remember it indexes start at 0.)
 
 # Code here
 # Example: my_authors[2]
 
+my_authors[0]
 
 # Now slice the list.
 
 # Code here
 # Example: my_authors[1:4]
 
+my_authors[2:5]
 
 ### Step 2 - Tuples ###
 
@@ -33,6 +36,8 @@ my_authors = []
 
 # Code here
 # Example: my_author_tuple = ("F. Scott Fitzgerald", "J.K. Rowling", "Ernest Hemingway", "Emily Dickenson", "George Orwell", "Ray Bradbury")
+
+author_tuple = ("Alfred Lansing", "David McCullough", "Brandon Sanderson", "J K Rowling", "Viktor Frankl", "Ralph Waldo Emerson", "James Clear")
 
 
 ### Step 3 - Sets ###
@@ -42,18 +47,22 @@ my_authors = []
 # Code here
 # Example: my_author_set = {"F. Scott Fitzgerald", "J.K. Rowling", "Ernest Hemingway", "Emily Dickenson", "George Orwell", "Ray Bradbury"}
 
-
+author_set = {"Alfred Lansing", "David McCullough", "Brandon Sanderson", "J K Rowling", "Viktor Frankl", "Ralph Waldo Emerson", "James Clear"}
 # Add a new author to your set.
 
 # Code here
 # Example: my_author_set.add("J.R.R. Tolkien")
 
+author_set.add("Brene Brown")
 
 # Try adding the same author again, and be sure to print your set.
+
 
 # Code here
 # Example: my_author_set.add("J.R.R. Tolkien")
 
+author_set.add("Alfred Lansing")
+# print(author_set)
 
 
 ### Step 4 - For Loops ###
@@ -65,10 +74,18 @@ my_authors = []
 
 # for book in my_authors:
     # print(book)
+for book in  my_authors:
+    print(book)
 
+print('----------------------')
 # for book in my_authors_tuple:
     # print(book)
+for book in author_tuple:
+    print(book)
 
+print('----------------------')
 # for book in my_authors_set:
     # print(book)
+for book in author_set:
+    print(book)
 
