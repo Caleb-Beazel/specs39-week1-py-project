e1d38feb85ced164466b6d50e2edcca5b7a1e58c
Caleb Beazel <caleb.beazel@gmail.com>
2023-06-10 12:57:52 -0600

    part 4 completed

--- part-4/part_4.py
+++ part-4/part_4.py
@@ -3,7 +3,23 @@
 ## Create five input statements to gather user's book they want to input to the system. After that be sure to turn it into a function.
 
 # Code here
+# def new_book():
 
+#     title = input("Title: ")
+#     author = input("Author: ")
+#     year = input("Year of Publication: ")
+#     rating = input("Rating: ")
+#     pages = input("Number of Pages: ")
+
+#     new_book_obj = {
+#         "title" : title,
+#         "author" : author,
+#         "year" : year,
+#         "rating": rating,
+#         "pages" : pages
+#     }
+
+#     return new_book_obj
 
 ### Step 2 - Type conversion
 
@@ -11,6 +27,23 @@
 
 # Code here
 
+# def new_book():
+
+#     title = input("Title: ")
+#     author = input("Author: ")
+#     year = int(input("Year of Publication: "))
+#     rating = float(input("Rating: "))
+#     pages = int(input("Number of Pages: "))
+
+#     new_book_obj = {
+#         "title" : title,
+#         "author" : author,
+#         "year" : year,
+#         "rating": rating,
+#         "pages" : pages
+#     }
+
+#     return new_book_obj
 
 
 ### Step 3 - Error handling
@@ -18,6 +51,36 @@
 ## Now take your previous function, and handle potential errors should the user type an answer that doesn't convert data-type properly.
 
 # Code here
+def new_book():
+
+
+
+    title = input("Please enter a title: ")
+    author = input("Please provide an author: ")
+    try:
+        year = int(input("provide a year of publication: "))
+    except:
+        year = int(input("provide a year of publication: "))
+    try:
+        rating = float(input("Please provide a rating between 1 and 5: "))
+    except:
+        rating = float(input("Please provide a rating between 1 and 5: "))
+    try:
+        pages = int(input("Please provide the number of pages: "))
+    except:
+        pages = int(input("Please provide the number of pages: "))
+        
+
+    book_dictionary = {
+        "title" : title,
+        "author" : author,
+        "year" : year,
+        "rating": rating,
+        "pages" : pages
+    }
+
+    return book_dictionary
+
 
 
 
@@ -26,6 +89,51 @@
 ## Now create a main menu function that gives the user options. Handle their choices with if/elif/else statements.
 
 # Code here
+book_list =[{
+    "title": "The Hunger Games",
+    "author": "Suzanne Collins",
+    "year": 2008,
+    "rating": 4.32,
+    "pages": 374
+},{
+    "title": "Man's Search For Meaning",
+    "author": "Viktor E. Frankl",
+    "year": 1959,
+    "rating": 5,
+    "pages": 144
+},{
+    "title": "Endurance",
+    "author": "Alfred Lansing",
+    "year": 1959,
+    "rating": 4.8,
+    "pages": 353
+}]
+
+
+# def main_menu():
+    
+#     menu = input("Show all books - 'list'   Add a book - 'add' ")
+#     if menu == 'list':
+#         print(book_list)
+#         to_main = input("Return to main menu? 'y' ")
+#         if to_main == 'y':
+#             main_menu()
+#     elif menu == 'add':
+#         book_list.append(new_book())
+#         print(book_list)
+#         to_main = input("Return to main menu? 'y' ")
+#         if to_main == 'y':
+#             main_menu()
+        
+# main_menu()
+#     title = None
+#     author = None
+#     year = None
+#     rating = None
+#     pages = None
+#  
+# if isinstance(year, int) == False:
+            # print("You must provide a year in numbers")
 
 
 ### Step 5 - while loops
@@ -34,3 +142,19 @@
 
 # Code here
 
+def main_menu():
+    exit_menu = False
+    while exit_menu == False:
+        menu = input("Show all books - 'list'   Add a book - 'add'   Quit book app - 'quit' ")
+        if menu == 'list':
+            print(book_list)
+            input("Hit enter to retun to main menu... ")
+        elif menu == 'add':
+            book_list.append(new_book())
+            print(book_list)
+            input("Hit enter to retun to main menu... ")
+            
+        elif menu == 'quit':
+            exit_menu = True
+        
+main_menu()
